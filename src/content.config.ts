a2da865774e030d77c91e251f25846e31edf015b
Thibaud Dervily <thibaud.dervily@gmail.com>
2025-01-08 22:35:43 +0100

    feat: fetch country from author

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
 import { defineCollection, z, reference } from "astro:content";
 import { glob, file } from "astro/loaders";
+import type { Country } from "./models/country";
 
 const posts = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/posts" }),
@@ -17,7 +18,27 @@ const authors = defineCollection({
     id: z.string(),
     name: z.string(),
     job: z.string(),
+    country: reference("countries"),
   }),
 });
 
-export const collections = { posts, authors };
+const countries = defineCollection({
+  loader: async () => {
+    const response = await fetch("https://api.first.org/data/v1/countries");
+
+    const { data } = (await response.json()) as Record<string, Country>;
+
+    return Object.entries(data).map(([key, country]: [string, Country]) => ({
+      id: key,
+      country: country.country,
+      region: country.region,
+    }));
+  },
+  schema: z.object({
+    id: z.string(),
+    country: z.string(),
+    region: z.string(),
+  }),
+});
+
+export const collections = { posts, countries, authors };
